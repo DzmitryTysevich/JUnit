875aaad96c36dfe8efb95baecce3a6bbe84af66d
DzmitryTysevich <dztysevich@gmail.com>
2021-01-15 22:56:39 +0300

    Module JUnit: task

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,14 +1,17 @@
+import org.apache.log4j.Logger;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
+    private static final Logger LOGGER = Logger.getLogger(TestRunner.class);
 
     public static void main(String[] args) {
+
         Result result = JUnitCore.runClasses(DiscriminantTest.class, RootOneTest.class, RootsTest.class, RootTwoTest.class);
         for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
+            LOGGER.info(failure.toString());
         }
-        System.out.println(result.wasSuccessful());
+        LOGGER.info(result.wasSuccessful());
     }
 }
